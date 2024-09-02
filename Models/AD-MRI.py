121d8ef205c49eb26b73bdc3e25a62ddb57a02ad
Danny <danielcle0708@gmail.com>
2024-09-01 20:06:20 -0400

    Updated AD-MRI.py and added Data directory with MRI data

--- Models/AD-MRI.py
+++ Models/AD-MRI.py
@@ -5,19 +5,40 @@
 
 ''' An AI Model that uses images of MRI brain scans to determine whether or not an individual has early onset Alzheimers disease '''
 
-import numpy as np
-import argparse
-import pdb
-import keras
+import numpy as np     # Advanced math functions
+import argparse        # Make command line arguments
+import pdb             # Debug the program
+import torch                                  # PyTorch (used for AI)
+#from torch import nn                          # Neural Network?
+#from torchvision import ImageFolder
+from collections import Counter
+import os              # To work with file paths
 
+
+# Command line arguments
 parser = argparse.ArgumentParser(description="Use an Ai model to detect early onset Alzheimer's disease")
 parser.add_argument('--debug', action='store_true', help='use pdb.set_trace() at end of program for debugging')
 parser.add_argument('--results', action='store_true', help='display the results of the model')
 
+# Conditional to determine if the computations should be run on the CPU or GPU
+# Idea: add a command line argument to choose GPU or CPU
+#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+dir_path = 'C:/Users/danie/Senior-Project/Models/Data'
+test_ds = os.path.join(dir_path, 'test')
+train_ds = os.path.join(dir_path, 'train')
+
+#train = ImageFolder(train_ds)
+#test = ImageFolder(test_ds)
+
+#class_map = {0:'Mild Demented', 1:'Moderate Demented', 2:'Non Demented', 3:'Very Mild Demented'}
+#class_dict = dict(Counter(train.targets))
+#class_dict
+
 def main(args): 
     
     
-    if args.results():
+    if args.results:
         get_results()
         
     if args.debug:
